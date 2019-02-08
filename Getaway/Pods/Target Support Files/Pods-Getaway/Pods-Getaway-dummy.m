6cf793d193f1b664ec1ace94d846f5ad1f454dba
singh422 <singh422@purdue.edu>
2019-02-08 00:48:56 -0500

    Initialized application with Firebase

---
+++ Getaway/Pods/Target Support Files/Pods-Getaway/Pods-Getaway-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Getaway : NSObject
+@end
+@implementation PodsDummy_Pods_Getaway
+@end
---
+++ Getaway/Pods/Target Support Files/Pods-GetawayTests/Pods-GetawayTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GetawayTests : NSObject
+@end
+@implementation PodsDummy_Pods_GetawayTests
+@end
