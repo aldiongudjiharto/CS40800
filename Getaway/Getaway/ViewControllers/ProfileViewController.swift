716949db4a0f20f942d2b24e241a7b429fa101cc
Dhriti Chawla <chawla5@purdue.edu>
2019-02-08 22:02:41 -0500

    Profile created

---
+++ Getaway/Getaway/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewController.swift
+//  Getaway
+//
+//  Created by Dhriti Chawla on 2/8/19.
+//  Copyright © 2019 Avinash Singh. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var myName: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var myUserName: UILabel!
+    @IBOutlet weak var myBio: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
