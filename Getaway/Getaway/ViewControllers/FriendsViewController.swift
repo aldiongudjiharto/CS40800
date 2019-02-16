bb3b1a3aa21241ad935a1fb27295f6a7bb8848d6
Dhriti Chawla <chawla5@purdue.edu>
2019-02-15 19:22:16 -0500

    made the search more interact

---
+++ Getaway/Getaway/ViewControllers/FriendsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FriendsViewController.swift
+//  Getaway
+//
+//  Created by Dhriti Chawla on 2/15/19.
+//  Copyright © 2019 Avinash Singh. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UIViewController {
+
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
